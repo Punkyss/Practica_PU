a71d216cdd95b0e084c81d950546e2e4a672acbb
gtd-fixe <gerard.trota2@gmail.com>
2021-01-11 18:47:04 +0100

    First commit to restart the project properly.

---
+++ src/medicalconsultation/TakingGuideline.java
@@ -0,0 +1,14 @@
+package medicalconsultation;
+
+public class TakingGuideline {
+    private dayMoment dMoment;
+    private float duration;
+    private String instructions;
+    private Posology posology;
+
+    public TakingGuideline(dayMoment dM, float du, String i, float d, float f, FqUnit u){
+        //. . .
+    } // Initializes attributes
+    //??? // the getters and setters
+
+}
