37c91a5364d4918bb1dda6450ac283eb37f5ecbf
gtd-fixe <gerard.trota2@gmail.com>
2021-01-12 10:05:56 +0100

    set some changes up

--- src/services/ScheduledVisitAgenda.java
+++ src/services/ScheduledVisitAgenda.java
@@ -6,7 +6,7 @@ import exceptions.HealthCardException;
 public class ScheduledVisitAgenda {
     HealthCardID card;
     private HealthCardID getHealthCardID() throws HealthCardException {
-        // chek if the card given == null
+        // chek if the card given got no id
         if(card.getPersonalID().equals(""))throw new HealthCardException("Not valid");
         return card;
     }
