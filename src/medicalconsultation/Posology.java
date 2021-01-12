fe0730d616672beb1260bbcd8159ae22736342b5
rogerLarriba <64427388+rogerLarriba@users.noreply.github.com>
2021-01-12 20:46:01 +0100

    Getters and Setters (Posology)

--- src/medicalconsultation/Posology.java
+++ src/medicalconsultation/Posology.java
@@ -7,12 +7,37 @@ public class Posology { // A class that represents the posology of a medicine
     private float freq;
     private FqUnit freqUnit;
 
+    // Initializes attributes
     public Posology (float d, float f, FqUnit u) {
         this.dose=d;
         this.freq=f;
         this.freqUnit=u;
         //. . .
-    } // Initializes attributes
+    }
 
-    //??? // the getters and setters
+    // the getters and setters
+
+    public float getDose() {
+        return dose;
+    }
+
+    public void setDose(float dose) {
+        this.dose = dose;
+    }
+
+    public float getFreq() {
+        return freq;
+    }
+
+    public void setFreq(float freq) {
+        this.freq = freq;
+    }
+
+    public FqUnit getFreqUnit() {
+        return freqUnit;
+    }
+
+    public void setFreqUnit(FqUnit freqUnit) {
+        this.freqUnit = freqUnit;
+    }
 }
