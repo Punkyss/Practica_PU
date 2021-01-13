7d3884870860084261c30293326d58294614fc21
rogerLarriba <64427388+rogerLarriba@users.noreply.github.com>
2021-01-13 20:32:36 +0100

    Tests Constructor, Getters i Setters de TakingGuidelineTest

---
+++ Tests/medicalconsultation/TakingGuidelineTest.java
@@ -0,0 +1,53 @@
+package medicalconsultation;
+
+import medicalconsultation.enumeration.dayMoment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static medicalconsultation.enumeration.dayMoment.*;
+import medicalconsultation.enumeration.FqUnit;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TakingGuidelineTest {
+    TakingGuideline preTest;
+    dayMoment dM;
+    FqUnit fqUnitTest;
+    String instructionTest;
+    Posology posologyTest;
+
+    @BeforeEach
+    void setUp(){
+        dM = BEFOREBREAKFAST;
+        fqUnitTest = FqUnit.MONTH;
+        instructionTest = "Prendre cada dia";
+        posologyTest = new Posology(2,3, fqUnitTest);
+        preTest = new TakingGuideline(dM, 6, instructionTest, 2, 3, fqUnitTest);
+    }
+
+
+    @Test
+    void takingGuidelineGettersTest(){
+        assertEquals(preTest.getdMoment(), dM);
+        assertEquals(preTest.getDuration(), 6);
+        assertEquals(preTest.getInstructions(), instructionTest);
+        assertEquals(preTest.getPosology().getDose(), posologyTest.getDose());
+        assertEquals(preTest.getPosology().getFreq(), posologyTest.getFreq());
+        assertEquals(preTest.getPosology().getFreqUnit(), posologyTest.getFreqUnit());
+    }
+
+    @Test
+    void takingGuidelineSettersTest(){
+        preTest.setdMoment(DURINGBREAKFAST);
+        preTest.setDuration(8);
+        preTest.setInstructions("Prendre una vegada al mes");
+        preTest.setPosology(new Posology(4,5,FqUnit.MONTH));
+
+        assertEquals(preTest.getdMoment(),DURINGBREAKFAST);
+        assertEquals(preTest.getDuration(), 8);
+        assertEquals(preTest.getInstructions(), "Prendre una vegada al mes");
+        assertEquals(preTest.getPosology().getDose(), 4);
+        assertEquals(preTest.getPosology().getFreq(), 5);
+        assertEquals(preTest.getPosology().getFreqUnit(), FqUnit.MONTH);
+    }
+
+}
