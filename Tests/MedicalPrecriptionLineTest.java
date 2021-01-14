bb1cb39e4982b7ef3abe7f1292e91ace390e1f3a
Spiritusrevenge <xavierlonca@gmail.com>
2021-01-14 19:56:04 +0100

    MedicalPrescritionLine

---
+++ Tests/MedicalPrecriptionLineTest.java
@@ -0,0 +1,45 @@
+import data.ProductID;
+import medicalconsultation.MedicalPrescriptionLine;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import medicalconsultation.TakingGuideline;
+import medicalconsultation.enumeration.FqUnit;
+import medicalconsultation.enumeration.dayMoment;
+import data.ProductID;
+import exceptions.EmptyIDException;
+import exceptions.NotValidCodeException;
+
+
+public class MedicalPrecriptionLineTest {
+    private dayMoment dMoment;
+    private float duration;
+    private String instructions;
+    private float dose;
+    private float freq;
+    private FqUnit freqUnit;
+    private ProductID product;
+    private TakingGuideline lineTest;
+    private MedicalPrescriptionLine medicalLineTest;
+
+    @BeforeEach
+    void setUp()throws NotValidCodeException, EmptyIDException{
+        dMoment=dayMoment.AFTERMEALS;
+        duration=5;
+        instructions="a";
+        dose=2;
+        freq=2;
+        freqUnit = FqUnit.DAY;
+        product=new ProductID("897789456123");
+        lineTest= new TakingGuideline(dMoment,duration,instructions,dose,freq,freqUnit);
+    }
+
+    @Test
+    void MedicalPrescriptionLineTest(){
+        medicalLineTest=new MedicalPrescriptionLine(product,lineTest);
+        assertEquals(lineTest,medicalLineTest.getInstructions());
+        assertEquals(product,medicalLineTest.getProduct());
+    }
+
+
+}
--- src/medicalconsultation/MedicalPrescriptionLine.java
+++ src/medicalconsultation/MedicalPrescriptionLine.java
@@ -1,5 +1,38 @@
 package medicalconsultation;
-
+import medicalconsultation.enumeration.FqUnit;
+import medicalconsultation.enumeration.dayMoment;
+import data.ProductID;
+/*private dayMoment dMoment;
+private float duration;
+private String instructions;
+    private float dose;
+    private float freq;
+    private FqUnit freqUnit;
+    dayMoment dMoment,float duration, String instructions, float dose, float freq, FqUnit freqUnit
+*/
 public class MedicalPrescriptionLine {
+    private ProductID product;
+    private TakingGuideline instructions;
+
+    public MedicalPrescriptionLine(ProductID product,TakingGuideline line){
+        this.product=product;
+        this.instructions= line;
+    }
+
+    public TakingGuideline getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(TakingGuideline instructions) {
+        this.instructions = instructions;
+    }
+
+    public ProductID getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductID product) {
+        this.product = product;
+    }
     // throws de cas que no estigui a (fa falta ferla nova)
 }
