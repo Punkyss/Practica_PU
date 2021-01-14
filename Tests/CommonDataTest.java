858ac74cf41c975aa9616f3574e71b6fdc50e188
gtd-fixe <gerard.trota2@gmail.com>
2021-01-14 19:30:44 +0100

    Final changes to the Data classes

--- Tests/CommonDataTest.java
+++ Tests/CommonDataTest.java
@@ -3,6 +3,7 @@ import data.HealthCardID;
 import data.ProductID;
 import exceptions.EmptyIDException;
 import exceptions.NotValidCodeException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,26 +13,37 @@ public class CommonDataTest  {
     ProductID productID;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws NotValidCodeException,EmptyIDException{
+
+        digitalSignature = new DigitalSignature(new byte[]{(byte) 0xe0,(byte)  0x4f});
+        healthCardID = new HealthCardID("BBBBBBBBQR648597807024000012");
+        productID = new ProductID("807024000012");
 
     }
 
     @Test
-    public void addIdTestEmpty() throws NotValidCodeException,EmptyIDException {
-        digitalSignature= new DigitalSignature(new byte[]{(byte) 0xe0,0x4f});
-        System.out.println("OK");
-        healthCardID= new HealthCardID("BBBBBBBBQR648597807024000012");
-        System.out.println("OK");
-        productID= new ProductID("807024000012");
-        System.out.println("OK");
+    public void addIdTestNotValidCodeException() {
+        Assertions.assertThrows(NotValidCodeException.class, () -> healthCardID= new HealthCardID("1BBBBBBBQR648597807024000012"));
+        Assertions.assertThrows(NotValidCodeException.class, () -> productID= new ProductID("W07024000012"));
     }
 
     @Test
-    public void addIdTestNotValid() throws NotValidCodeException,EmptyIDException {
-        digitalSignature= new DigitalSignature(new byte[]{});
-        healthCardID= new HealthCardID("");
-        productID= new ProductID("");
+    public void addIdTestEmptyIDException() {
+        Assertions.assertThrows(EmptyIDException.class, () -> digitalSignature= new DigitalSignature(new byte[]{}));
+        Assertions.assertThrows(EmptyIDException.class, () -> healthCardID= new HealthCardID(""));
+        Assertions.assertThrows(EmptyIDException.class, () -> productID= new ProductID(""));
+    }
 
+    @Test
+    public void addIdTestValid() {
+        byte[] tempByte = new byte[]{(byte) 0xe0,(byte)  0x4f};
+        int count=0;
+        while (count != 2) {
+            Assertions.assertEquals(digitalSignature.getSignatureCode()[count], tempByte[count]);
+            count++;
+        }
+        Assertions.assertEquals(healthCardID.getCIP(), "BBBBBBBBQR648597807024000012");
+        Assertions.assertEquals(productID.getCode(), "807024000012");
     }
 
 
--- src/data/DigitalSignature.java
+++ src/data/DigitalSignature.java
@@ -3,16 +3,15 @@ package data;
 import exceptions.EmptyIDException;
 import exceptions.NotValidCodeException;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class DigitalSignature {
     private final byte[] signatureCode;
     // 2 brand new exceptions (NotValidCodeException for empty codes) (EmptyIDException for not valid ones)
-    public DigitalSignature(byte[] signatureCode) throws NotValidCodeException, EmptyIDException {
-        for (byte b:signatureCode) {
-            if(Byte.valueOf(b)!=null)throw new NotValidCodeException("A character is not a byte");
-        }
-        if(signatureCode.equals("")) throw new EmptyIDException("Code is not valid");
+
+    public DigitalSignature(byte[] signatureCode) throws EmptyIDException {
+        if(signatureCode.length==0) throw new EmptyIDException("Signature code is empty");
         this.signatureCode = signatureCode;
     }
 
@@ -25,19 +24,18 @@ public class DigitalSignature {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         DigitalSignature that = (DigitalSignature) o;
-        return Objects.equals(signatureCode, that.signatureCode);
+        return Arrays.equals(signatureCode, that.signatureCode);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(signatureCode);
+        return Arrays.hashCode(signatureCode);
     }
 
     @Override
     public String toString() {
         return "DigitalSignature{" +
-                "signatureCode='" + signatureCode + '\'' +
+                "signatureCode=" + Arrays.toString(signatureCode) +
                 '}';
     }
-
 }
